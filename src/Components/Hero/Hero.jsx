25826687ab577f884295f10c50a5670994bb8cee
KavishkaJayakody <jayakodykavishka@gmail.com>
2024-06-19 10:59:37 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className='hero-text'>
+            <h1>Better education for a better world</h1>
+            <p>We provide a better educational experience preparing you for the dynamic future through 
+                skills, knowledge and experience to excel in academic and professional world
+            </p>
+            <button className='btn'>Explore more<img src={dark_arrow} alt=''/></button>
+        
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
